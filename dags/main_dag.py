c4ee1e2e98c6fc834c909a4066eecefe7ddb4670
Harun Mbaabu <51136314+HarunMbaabu@users.noreply.github.com>
2023-07-31 12:35:07 +0300

    Debug main_dafs.py || crawler dag

--- dags/main_dag.py
+++ dags/main_dag.py
@@ -1,33 +1,51 @@
-from airflow import DAG  
-from datetime import datetime, timedelta 
-from airflow.operators.bash import BashOperator 
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.python_operator import PythonOperator
 
 default_args = {
-    'owner': 'airflow', 
-	'depends_on_past' : 'False', 
-	'start_date': datetime(2023,5,10),
-	'retries': 1, 
-	'retry_delay': timedelta(minutes=5)
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 5, 10),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
 }
 
+dag = DAG(
+    "crawler",
+    default_args=default_args,
+    description="Simple Crawler To Get Data",
+    schedule_interval=timedelta(days=7),  # Set it to 7 days
+    catchup=False
+)
 
-dag  = DAG( 
-    "crawler", 
-    default_args = default_args, 
-    description = "Simple Crawler To Get Data", 
-    schedule = timedelta(days=1),
-    catchup = False
-) 
 
-t1 = BashOperator( 
-    task_id = "run_crawler",  
-    bash_command = "./scripts/bs_rent_crawler.py",
-    dag = dag
-                ) 
-t2 = BashOperator( 
-    task_id = "clean_data",  
-    bash_command = "./scripts/clean_rent_data.py",
-    dag = dag
-                )
+def run_bs_buy_crawler():
+    
+    command = "python ./web-scrapers/bs_buy_crawler.py"
+    import subprocess
 
-t1 >>  t2
+    subprocess.run(command, shell=True)
+
+
+def run_bs_rent_crawler():
+    command = "python ./web-scrapers/bs_rent_crawler.py"
+    import subprocess
+
+    subprocess.run(command, shell=True)
+
+
+# Define tasks to run the crawlers using PythonOperator
+run_bs_buy_crawler_task = PythonOperator(
+    task_id='run_bs_buy_crawler_task',
+    python_callable=run_bs_buy_crawler,
+    dag=dag
+)
+
+run_bs_rent_crawler_task = PythonOperator(
+    task_id='run_bs_rent_crawler_task',
+    python_callable=run_bs_rent_crawler,
+    dag=dag
+)
+
+# Set the task dependencies
+run_bs_buy_crawler_task >> run_bs_rent_crawler_task
